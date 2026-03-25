8660483b306a407256c54fb3a4f3ffb628d2cecd
Deepak Poly <deepakpoly099@gmail.com>
2026-03-25 02:53:51 -0600

    fix Jpa Repo Scan

--- src/main/java/com/realtimecode/DemoApplication.java
+++ src/main/java/com/realtimecode/DemoApplication.java
@@ -2,8 +2,10 @@ package com.realtimecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.realtimecode.repository")
 public class DemoApplication {
 
 	public static void main(String[] args) {
